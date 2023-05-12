b0e5c82a3445bf12aa12fb56ef4c9f15f0b56422
windbro <cloudyrain1998@gmail.com>
2023-05-12 11:27:16 +0800

    chen_run.sh added, to simplify the running command

---
+++ cifar100-class-incremental/chen_run.sh
@@ -0,0 +1,13 @@
+for i in 2 5
+do
+{
+CUDA_VISIBLE_DEVICES=0 python class_incremental_cosine_cifar100.py \
+    --nb_cl_fg 50 --nb_cl $i --nb_protos 20 \
+    --resume --rs_ratio 0.0 --imprint_weights \
+    --less_forget --lamda 5 --adapt_lamda \
+    --random_seed 1993 \
+    --mr_loss --dist 0.5 --K 2 --lw_mr 1 \
+    --ckp_prefix seed_1993_rs_ratio_0.0_class_incremental_MR_LFAD_cosine_cifar100 \
+    2>&1 | tee log_seed_1993_rs_ratio_0.0_class_incremental_MR_LFAD_cosine_cifar100_nb_cl_fg_50_nb_cl_($i)_nb_protos_20.txt
+}&
+done
